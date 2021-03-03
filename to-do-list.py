df5768cecc2d34dbc6001e192a4ada8bb747f1f4
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-03-03 15:59:21 -0300

    Update to-do-list.py

--- to-do-list.py
+++ to-do-list.py
@@ -2,15 +2,27 @@ from dearpygui.core import *
 from dearpygui.simple import *
 
 set_main_window_size(400, 400)
-
+index = ['None']
 def callback(sender, data, i):
-    with window('To do list'):
+    with window('Lista de Tarefas'):
         i = get_value('Tarefa')
-        add_text(i)
+        t = get_value('Tarefa')
+        index.append(i)
+        add_text(f"{index.index(i)} {t}")
 
-with window('To do list'):
+def clear(sender, data):
+    delete_item('Lista de Tarefas')
+    with window('Lista de Tarefas', width=400, height=400):
+        set_window_pos('Lista de Tarefas', 0, 0)
+        add_input_text('Tarefa')
+        add_button('Adicione', callback=callback)
+        add_button('Limpar', callback=clear)
+
+with window('Lista de Tarefas', width=400, height=400):
+    set_window_pos('Lista de Tarefas', 0, 0)
     add_input_text('Tarefa')
     add_button('Adicione', callback=callback)
+    add_button('Limpar', callback=clear)
 
 
-start_dearpygui(primary_window='To do list')
+start_dearpygui()
