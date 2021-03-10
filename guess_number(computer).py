eea7009a94ffad46c6b17ca8e201778d860f33c8
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-03-10 11:20:20 -0300

    Update guess_number(computer).py

--- guess_number(computer).py
+++ guess_number(computer).py
@@ -3,15 +3,14 @@ from random import randint
 def guess_number():
     low_number = 1
     high_number = 10
-    guess = randint(low_number, high_number)
-    print(guess)
-    hint = input("Are I right? if not, more(m) or less(l) (c for correct)")
-    while guess != "c":
+    hint = ''
+    while hint != "c":
         if low_number != high_number:
             guess = randint(low_number, high_number)
         else:
             guess = low_number
-        if hint == "m":
+        hint = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if hint == "h":
             high_number = guess - 1
             guess_number()
         elif hint == "l":
