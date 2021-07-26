0356e4d040f3f062f66ca7b996bc3387c5d41fd7
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-07-26 10:19:55 -0300

    Conversor de unidades termométricas

---
+++ termometrica.py
@@ -0,0 +1,23 @@
+def celsius_to_fahrenheit(Celsius):
+    F = (Celsius/5)*9 + 32
+    print(F)
+
+def celsius_to_kelvin(Celsius):
+    K = Celsius + 273
+    print(K)
+
+def kelvin_to_celsius(Kelvin):
+    C = Kelvin - 273
+    print(C)
+
+def kelvin_to_fahrenheit(Kelvin):
+    F = (((Kelvin - 273) / 5) * 9) + 32
+    print(F)
+
+def fahrenheit_to_kelvin(Fahrenheit):
+    K = (((Fahrenheit - 32) / 9) * 5) + 273
+    print(K)
+
+def fahrenheit_to_celsius(Fahrenheit):
+    C = ((Fahrenheit - 32) / 9) * 5
+    print(C)
