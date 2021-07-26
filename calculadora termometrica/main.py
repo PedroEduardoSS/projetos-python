f6d8329aafd1556309654b10d6940ea3e83a48c1
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-07-26 11:15:18 -0300

    Interface Gráfica de um conversor termométrico

---
+++ calculadora termometrica/main.py
@@ -0,0 +1,41 @@
+from dearpygui.dearpygui import *
+from functions import *
+
+setup_viewport()
+set_viewport_title(title='termocalc')
+set_viewport_width(200)
+set_viewport_height(500)
+
+conversoes = ["Celsius para Fahrenheit", "Celsius para Kelvin", "Fahrenheit para Celsius", "Fahrenheit para Kelvin", "Kelvin para Celsius", "Kelvin para Fahrenheit"]
+
+def converter():
+    temperatura = get_value(valor)
+    item = get_value(items)
+    if item == "Celsius para Fahrenheit":
+        result = celsius_to_fahrenheit(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+    elif item == "Celsius para Kelvin":
+        result = celsius_to_kelvin(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+    elif item == "Fahrenheit para Celsius":
+        result = fahrenheit_to_celsius(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+    elif item == "Fahrenheit para Kelvin":
+        result = fahrenheit_to_kelvin(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+    elif item == "Kelvin para Celsius":
+        result = kelvin_to_celsius(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+    elif item == "Kelvin para Fahrenheit":
+        result = kelvin_to_fahrenheit(temperatura)
+        configure_item(resultado, default_value=f"{result}")
+
+with window(label="TermoCalc", width=250, height=400):
+    add_text("Escolha a conversão", bullet=True)
+    add_separator()
+    valor = add_input_int(min_value=-10000, max_value=10000)
+    items = add_radio_button(items=conversoes)
+    add_button(label="Converter", callback=converter)
+    resultado = add_text("")
+
+start_dearpygui()
