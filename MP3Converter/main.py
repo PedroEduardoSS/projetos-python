825c3fb03bb4310451a4aa1596bbac5296b51db1
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-02-04 15:16:36 -0300

    update MP3 converter

--- MP3Converter/main.py
+++ MP3Converter/main.py
@@ -3,24 +3,30 @@ from pytube import YouTube
 import os
 
 dpg.create_context()
-dpg.create_viewport(title='MP3Converter', width=400, height=200)
+dpg.create_viewport(title='MP3Converter', width=600, height=500)
 
-def salvar():
+
+def callback(sender, app_data):
+    directory = app_data.get('file_path_name')
     link = dpg.get_value('link')
     yt = YouTube(link)
     audio = yt.streams.filter(only_audio=True).first()
     
-    #destination
-    out_file = audio.download()
+    out_file = audio.download(output_path=directory)
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
+    with dpg.window(label="Sucesso", width=600, height=100, tag="popup"):
+        dpg.add_text(f"Arquivo baixado em:{directory}")
+        dpg.add_button(label="Fechar", callback=lambda: dpg.delete_item("popup"))
     
-with dpg.window(label="Main", width=400, height=200):
+dpg.add_file_dialog(height=400, directory_selector=True, show=False, callback=callback, tag="file_dialog_id")
+    
+with dpg.window(label="Main", width=600, height=500):
     dpg.add_input_text(
         hint="Link do video",
         tag='link')
-    dpg.add_button(label="Salve", callback=salvar)
+    dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
